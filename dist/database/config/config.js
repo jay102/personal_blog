241f2b6abc690deba08627e87e8b921bfed2816a
James <jordunjames@gmail.com>
2019-10-24 16:45:35 +0100

    removed config from env

---
+++ dist/database/config/config.js
@@ -0,0 +1,41 @@
+"use strict";
+const Sequelize = require('sequelize');
+require('dotenv').config();
+const env = process.env.NODE_ENV || 'development';
+let db;
+if (env === "test") {
+    db = new Sequelize('jaycodes_test', 'postgres', '123ifeco', {
+        host: 'localhost',
+        dialect: 'postgres',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+}
+else if (env === "development") {
+    db = new Sequelize('jaycodes', 'postgres', '123ifeco', {
+        host: 'localhost',
+        dialect: 'postgres',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+}
+else {
+    db = new Sequelize(`${process.env.DATABASE_URL}`, {
+        dialect: 'postgres',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+}
+module.exports = db;
